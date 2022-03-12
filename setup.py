eef16be52349a8165bf7e9c7bdb638f0e65499e8
Loraine Brillet-Guéguen <loraine-gueguen@users.noreply.github.com>
2022-03-12 11:42:11 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='RREFinder',
+    scripts=['RRE.py'],
+    description='Bioinformatic application for the detection of RREs in protein sequences of interest',
+    url='https://github.com/Alexamk/RREFinder',
+    )
