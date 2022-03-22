10bd3a6aa8c83962f0e69cbd46bf7ae3e48e42f5
Alexander <alexander.kloosterman@ki.se>
2022-03-22 19:41:33 +0100

    Added author information

--- setup.py
+++ setup.py
@@ -4,7 +4,9 @@ from distutils.core import setup
 
 setup(
     name='RREFinder',
-    scripts=['RRE.py'],
+    scripts=['RRE.py', 'download_RRE_databases.py', 'setup_RRE_exploratory.py'],
     description='Bioinformatic application for the detection of RREs in protein sequences of interest',
     url='https://github.com/Alexamk/RREFinder',
+    author='Alexander Kloosterman',
+    author_email='alexander.kloosterman@ki.se',
     )
