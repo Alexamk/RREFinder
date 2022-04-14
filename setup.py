719d622e2cf6c4b30c7d1482dc6917d7a0587f7a
Loraine Brillet-Guéguen <loraine-gueguen@users.noreply.github.com>
2022-04-14 22:12:30 +0200

    new package setup

--- setup.py
+++ setup.py
@@ -3,22 +3,24 @@
 import os
 from distutils.core import setup
 
-def generate_data_files():
+def generate_package_data():
     data_files = []
     for directory in ['data','lib','sample_input']:
         for path, dirs, files in os.walk(directory):
-            install_dir = os.path.join('bin',path)
-            list_entry = (install_dir, [os.path.join(path, f) for f in files ])
-            data_files.append(list_entry)
-    data_files.append(('bin',['config.ini']))
-    return data_files
+            for f in files:
+                data_files.append(os.path.join(path, f))
+    data_files.append('config.ini')
+    package_data={'BiG-SCAPE': data_files}
+    return package_data
 
 setup(
     name='RREFinder',
     version='v1.0.1',
     py_modules=['RRE','download_RRE_databases','setup_RRE_exploratory'],
     scripts=['RRE.py','download_RRE_databases.py','setup_RRE_exploratory.py'],
-    data_files=generate_data_files(),
+    packages=['RREFinder'],
+    package_dir={'RREFinder': '.'},
+    package_data=generate_package_data(),
     description='Bioinformatic application for the detection of RREs in protein sequences of interest',
     url='https://github.com/Alexamk/RREFinder',
     author='Alexander Kloosterman',
